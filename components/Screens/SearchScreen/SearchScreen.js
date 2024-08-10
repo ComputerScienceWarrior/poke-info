b30c95dc326d966688c1bf8c02caf60093836141
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-10 08:42:07 -0700

    creates SearchScreen component

---
+++ components/Screens/SearchScreen/SearchScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, Input } from 'react-native';
+
+const SearchScreen = () => {
+    return(
+        <View>
+            <Input placeholder="Search for a Pokemon..."/>
+        </View>
+    );
+};
+
+export default SearchScreen;
