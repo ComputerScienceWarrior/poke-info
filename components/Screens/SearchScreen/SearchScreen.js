1b004f1c8df6f1b9b01a89f920e396aecf78f549
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-10 12:09:29 -0700

    adds search button

--- components/Screens/SearchScreen/SearchScreen.js
+++ components/Screens/SearchScreen/SearchScreen.js
@@ -1,10 +1,15 @@
-import { SafeAreaView, Text, TextInput } from 'react-native';
+import { SafeAreaView, Text, TextInput, Button } from 'react-native';
 import Styles from './styles';
+import { useState } from 'react';
+import axios from 'axios';
 
 const SearchScreen = () => {
+    const [result, setResult] = useState('')
+
     return(
-        <SafeAreaView>
+        <SafeAreaView style={Styles.container}>
             <TextInput style={Styles.input} placeholder="Search for a Pokemon..."/>
+            <Button style={Styles.searchButton} title="Search" onPress={() => console.log('hi')} />
         </SafeAreaView>
     );
 };
