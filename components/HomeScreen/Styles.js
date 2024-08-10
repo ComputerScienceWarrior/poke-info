b7b9be61b89a7a862c7b5594b2e4cc510ce8e421
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-10 08:15:42 -0700

    creates external file for HomeScreen styles

---
+++ components/HomeScreen/Styles.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet, } from "react-native";
+
+const Styles = StyleSheet.create({ 
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        height: '100%',
+        width: '100%'
+    },
+    homeScreenText: {
+        color: 'blue',
+        fontSize: 16,
+        letterSpacing: 2
+    }
+});
+
+export default Styles;
