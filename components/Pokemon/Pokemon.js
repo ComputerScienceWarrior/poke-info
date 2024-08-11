0da437561231a2782875b060061300cb4e78a036
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-10 23:34:07 -0700

    fix Pokemon card UI; properly display pokemon image; prevents image and search fucntionality from conflicting

--- components/Pokemon/Pokemon.js
+++ components/Pokemon/Pokemon.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from "react-native";
-import Styles from "./styles";
+import { SafeAreaView, Text, View, Image, KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from "react-native";
 import HorizontalLine from '../HorizontalLine/HorizontalLine';
+import styles from './styles'
 
 const Pokemon = (props) => {
     return (
-        <SafeAreaView style={Styles.container}>
-            <View style={Styles.cardHeader}>
-                <View>
-                    <Text style={Styles.headerText}>{props.name}</Text>
-                    <Text style={Styles.headerText}>ID No.{props.id}</Text>
-                </View>
-            </View>
-            <HorizontalLine />
-            <View>
-                <Text style={Styles.detailsText}>Height: {props.height} ft.</Text>
-            </View>
-            <View>
-                <Text style={Styles.detailsText}>Weight: {props.weight} lbs.</Text>
-            </View>
-        </SafeAreaView>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+        >
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <SafeAreaView style={styles.innerContainer}>
+                    <View style={styles.cardHeader}>
+                        <Text style={styles.headerText}>{props.name}</Text>
+                        <Text style={styles.headerText}>ID No.{props.id}</Text>
+                    </View>
+                    <HorizontalLine />
+                    <View style={styles.imagesContainer}>
+                        <Image 
+                            source={{ uri: props.image }}
+                            style={styles.image}
+                        />
+                    </View>
+                    <View style={styles.detailsContainer}>
+                        <Text style={styles.detailsText}>Height: {props.height} ft.</Text>
+                        <Text style={styles.detailsText}>Weight: {props.weight} lbs.</Text>
+                    </View>
+                </SafeAreaView>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
