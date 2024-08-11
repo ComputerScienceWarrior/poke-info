28eef82759cf40f9626a43a164b40c690782afff
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-11 07:47:05 -0700

    updates styling to have a better colorScheme

--- components/Screens/HomeScreen/Styles.js
+++ components/Screens/HomeScreen/Styles.js
@@ -1,14 +1,34 @@
 import { View, Text, StyleSheet, } from "react-native";
 
-const Styles = StyleSheet.create({ 
+const styles = StyleSheet.create({ 
     container: {
         display: 'flex',
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'antiquewhite',
+        backgroundColor: 'white',
         height: '100%',
         width: '100%'
     },
+    searchButton: {
+        width: 200,
+        borderRadius: 10,
+        borderColor: 'black',
+        padding: 10,
+        backgroundColor: 'lightblue',
+        margin: 20
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontStyle: 'italic',
+        letterSpacing: 1
+    },
+    mainImage: {
+        width: 175,
+        height: 175,
+        marginBottom: 50
+    },
 });
 
-export default Styles;
+export default styles;
