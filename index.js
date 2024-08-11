6f3f6e3628dc8ce544eff5a77076c2c0dddbf2aa
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-10 20:10:25 -0700

    imports Horizontal line to main export file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as HomeScreen } from './components/Screens/HomeScreen/HomeScreen';
 export { default as SearchScreen } from './components/Screens/SearchScreen/SearchScreen';
 export { default as Pokemon } from './components/Pokemon/Pokemon';
+export { default as HorizontalLine } from './components/HorizontalLine/HorizontalLine';
